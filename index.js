b1d846bd43ea964658d0b7fc7743614b5a7d5ff7
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 19:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function Scooter(year, color, model){
   
   return year , color , model;
 }
+
+
+function Driver(name, age, ex){
+  
+  this.name = name;
+  this.age = age;
+  this.ex = ex;
+   
+  
+}
